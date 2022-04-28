58edb155378adcdc10e5b1276e9ad090d29d37eb
Huray-hub <panos.skiadas12@gmail.com>
2022-04-29 00:29:09 +0300

    minor updates

---
+++ scripts/.local/bin/bt-toggle-headphones.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+is_bt_device_connected() {
+	local deviceInfo=$1
+
+	[[ "$(echo "$deviceInfo" | grep Connected | cut -d ":" -f2)" == " yes" ]] && return 0
+
+	return 1
+}
+
+get_device_name() {
+	local deviceInfo=$1
+
+	echo "$deviceInfo" | grep Name | cut -d ":" -f2
+}
+
+main() {
+	local deviceUid=AC:1D:06:41:84:5D
+	local deviceInfo
+	deviceInfo=$(bluetoothctl info "$deviceUid")
+
+	is_bt_device_connected "$deviceInfo" && action=disconnect || action=connect
+
+	local deviceName
+	deviceName=$(get_device_name "$deviceInfo")
+
+	bluetoothctl "$action" "$deviceUid" && notify-send "$deviceName ${action}ed successfully"
+
+	return 0
+}
+
+main
