9be55f1c364979c1ac3899e70bb828431ee9cb1a
Huray-hub <panos.skiadas12@gmail.com>
2022-05-01 03:07:18 +0300

    added nvim colorizer (display colors on hexcodes)

--- nvim/.config/nvim/lua/huray/plugins.lua
+++ nvim/.config/nvim/lua/huray/plugins.lua
@@ -126,6 +126,9 @@ return packer.startup(function(use)
   -- Competitive programming
   use('p00f/cphelper.nvim')
 
+  -- Colors
+  use('norcalli/nvim-colorizer.lua')
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
